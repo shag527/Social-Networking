1dd7f4947c4bcca82d831421ff3735187665813a
shag527 <shagunkatoch527@gmail.com>
2021-03-03 13:26:20 +0530

    Third Commit

--- server.js
+++ server.js
@@ -44,10 +44,3 @@ http.listen(PORT,function(){
         });
     });
 });
-
-if (process.env.NODE_ENV === "production"{
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-    });
-  }
