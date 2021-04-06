4a91df39e8149aa89a54271d3f036351851ea023
basketcase03 <muskankhajuria03@gmail.com>
2021-04-07 01:55:07 +0530

    addded home

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -2,7 +2,48 @@ import React from 'react'
 
 const Home = ()=>{
     return (
-        <h1>home</h1>
+        <div className="home">
+            <div className="card home-card">
+                <h5>User Name</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1605640840605-14ac1855827b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1491&q=80"/>
+                </div>
+                <div className="card-content">
+                <i class="material-icons">favorite</i>
+                    <h6>Title</h6>
+                    <p>This was an amazing place</p>
+                    <input type="text" placeholder="add comment"/>
+                </div>
+            </div>
+
+            <div className="card home-card">
+                <h5>User Name</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1605640840605-14ac1855827b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1491&q=80"/>
+                </div>
+                <div className="card-content">
+                <i class="material-icons">favorite</i>
+                    <h6>Title</h6>
+                    <p>This was an amazing place</p>
+                    <input type="text" placeholder="add comment"/>
+                </div>
+            </div>
+
+            <div className="card home-card">
+                <h5>User Name</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1605640840605-14ac1855827b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1491&q=80"/>
+                </div>
+                <div className="card-content">
+                <i class="material-icons">favorite</i>
+                    <h6>Title</h6>
+                    <p>This was an amazing place</p>
+                    <input type="text" placeholder="add comment"/>
+                </div>
+            </div>
+
+
+        </div>
     )
 
 }
